768b26dd0c00818c7e0f4ac2cc003c7801155315
Kieran Campbell <kieranrcampbell@gmail.com>
2016-03-23 11:36:33 +0000

    Package provisionally working

---
+++ R/bnlfa.R
@@ -0,0 +1,53 @@
+## Bayesian non-linear factor analysis for incorporating 
+## prior knowledge into single-cell trajectory learning
+## kieranc@well.ox.ac.uk
+
+library(MCMCglmm)
+library(coda)
+library(rstan)
+
+
+
+#' Fit Bayesian non-linear factor analysis
+#' 
+#' @param Y Cell-by-gene (N by G) logged expression matrix
+#' @param k_means G mean activation strength parameters
+#' @param t0_means G mean activation time parameters
+#' @param k_sd Optional standard deviations for k parameters
+#' @param t0_sd Optional standard deviations for t0 parameters
+#' 
+#' @returns ...
+bnlfa <- function(Y, k_means, t0_means,
+                  k_sd = rep(1, ncol(Y)), 
+                  t0_sd = rep(1, ncol(Y)),
+                  ...) {
+  G <- ncol(Y)
+  N <- nrow(Y)
+  
+  ## sanity checking
+  stopifnot(length(k_means) == length(t0_means))
+  stopifnot(length(k_means) == G)
+  
+  ## stan setup
+  data <- list(Y = t(Y), G = G, N = N, 
+               k_means = k_means, k_sd = k_sd,
+               t0_means = t0_means, t0_sd = t0_sd)
+  
+  stanfile <- system.file("bnlfa.stan", package = "bnlfa")
+  model <- stan_model(stanfile)
+  
+  ## manipulate stan defaults
+  stanargs <- list(...)
+  if(!('iter' %in% names(stanargs))) stanargs$iter <- 1e4
+  if(!('chains' %in% names(stanargs))) stanargs$chains <- 1
+  if(!('thin' %in% names(stanargs))) stanargs$thin <- 5
+  stanargs$object <- model
+  stanargs$data <- data
+  
+  ## call sampling
+  fit <- do.call(sampling, stanargs)
+  
+  ## get pseudotime map
+  tmap <- posterior.mode(mcmc(extract(fit, "t")$t))
+  return(list(fit = fit, tmap = tmap))
+}
---
+++ inst/bnlfa.stan
@@ -0,0 +1,55 @@
+
+data {
+  int<lower = 2> N; // number of cells
+  int<lower = 2> G; // number of genes
+  
+  vector[N] Y[G]; // matrix of gene expression values
+  
+  real k_means[G]; // mean parameters for k provided by user
+  real k_sd[G]; // standard deviation parameters for k provided by user
+  
+  real t0_means[G]; // mean parameters for t0 provided by user
+  real t0_sd[G]; // standard deviation parameters for t0 provided by user
+
+}
+
+parameters {
+  // parameters we'll let stan infer
+  real<lower = 0> mu0[G];
+  real<lower = 0> tau[G]; // precision
+  
+  // parameters with user-defined priors
+  real k[G];
+  real t0[G];
+  
+  real<lower = 0, upper = 1> t[N]; // pseudotime of each cell
+
+
+}
+
+transformed parameters {
+  vector[N] mu[G]; // mean for cell i gene g
+  for(g in 1:G) {
+    for(i in 1:N) mu[g][i] <- 2 * mu0[g] / (1 + exp(-k[g] * (t[i] - t0[g])));
+  }
+}
+
+model {
+  // user defined priors
+  for(g in 1:G) {
+    k[g] ~ normal(k_means[g], k_sd[g]);
+    t0[g] ~ normal(t0_means[g], t0_sd[g]);
+  }
+  
+  // model priors
+
+  mu0 ~ normal(0, 1);
+  for(g in 1:G) tau[g] ~ gamma(1.0, 1.0);
+  for(i in 1:N) t[i] ~ normal(0.5, 1);
+  
+  for(g in 1:G) {
+    for(i in 1:N) {
+      Y[g][i] ~ normal(mu[g][i], 1 / sqrt(tau[g]));
+    }
+  }
+}
