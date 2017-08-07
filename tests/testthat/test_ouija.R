037da0e6c8a8618effdc679f4a91f66256e29c99
Kieran Campbell <kieranrcampbell@gmail.com>
2017-08-07 21:42:53 +0100

    Add some actual unit tests

--- tests/testthat/test_ouija.R
+++ tests/testthat/test_ouija.R
@@ -6,15 +6,23 @@ context("Basic functionality")
 
 test_that("ouija returns valid object", {
   data(example_gex)
-  
-  #suppressWarnings(oui <- ouija(synth_gex, iter = 200, warn_lp = FALSE))
-  
-  if(FALSE) { # Change back when finished dev
-  oui <- ouija(example_gex[1:20, 1:4], iter = 200, warn_lp = FALSE)
+  G <- 4
+  N <- 50
+  small_example_gex <- example_gex[seq_len(N), seq_len(G)] # reduce to small expression matrix
+
+  oui <- ouija(small_example_gex, iter = 500)
   
   expect_is(oui, "ouija_fit")
   expect_is(oui$fit, "stanfit")
-  expect_equal(dim(example_gex), dim(oui$Y))
-  expect_equal(dim(example_gex)[1], length(map_pseudotime(oui)))
-  }
+  expect_equal(dim(small_example_gex), dim(oui$Y))
+  expect_equal(N, length(map_pseudotime(oui)))
+  expect_equal(G, length(switch_strengths(oui)))
+  expect_equal(G, length(switch_times(oui)))
+  
+  reg_df <- gene_regulation(oui)
+  expect_is(reg_df, "data.frame")
+  
+  cmat <- consistency_matrix(oui)
+  expect_is(cmat, "matrix")
+  expect_equal(dim(cmat), c(N, N))
 })
